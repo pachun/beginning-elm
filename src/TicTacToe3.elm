0e13efb08e61e7afb89738fa4c6257c23600f7d8
Nick Pachulski <nick@pachulski.me>
2022-10-25 16:48:06 -0400

    wip

--- src/TicTacToe3.elm
+++ src/TicTacToe3.elm
@@ -5,12 +5,14 @@ import Browser
 import Html exposing (Html)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import List.Extra
 
 
 
 -- MAIN
 
 
+main : Program () Board BrowserInteraction
 main =
     Browser.sandbox
         { init = initialBoard
@@ -23,14 +25,14 @@ main =
 -- MODEL
 
 
-type BoardSquare
+type BoardSquareValue
     = X
     | O
     | Empty
 
 
 type alias Board =
-    Array (Array BoardSquare)
+    Array (Array BoardSquareValue)
 
 
 emptyBoard : Int -> Board
@@ -38,10 +40,12 @@ emptyBoard boardSize =
     Array.repeat boardSize (Array.repeat boardSize Empty)
 
 
+initialBoardSize : Int
 initialBoardSize =
-    3
+    5
 
 
+initialBoard : Board
 initialBoard =
     emptyBoard initialBoardSize
 
@@ -56,20 +60,170 @@ type alias BoardSquarePosition =
 
 type BrowserInteraction
     = Clicked BoardSquarePosition
+    | ClickedReset
 
 
 updateBoard : BrowserInteraction -> Board -> Board
 updateBoard browserInteraction board =
+    case browserInteraction of
+        Clicked ( row, col ) ->
+            case Array.get row board of
+                Just boardRow ->
+                    case Array.get col boardRow of
+                        Just boardSquareValue ->
+                            if boardSquareValue == Empty then
+                                Array.set row (Array.set col (nextTurn board) boardRow) board
+
+                            else
+                                board
+
+                        Nothing ->
+                            board
+
+                Nothing ->
+                    board
+
+        ClickedReset ->
+            initialBoard
+
+
+boardAsLists : Board -> List (List BoardSquareValue)
+boardAsLists board =
     board
+        |> Array.toList
+        |> List.map (\row -> Array.toList row)
+
+
+nextTurn : Board -> BoardSquareValue
+nextTurn board =
+    let
+        pastTurns =
+            List.concat (boardAsLists board)
+
+        numberOfXsTurns =
+            pastTurns
+                |> List.filter ((==) X)
+                |> List.length
+
+        numberOfOsTurns =
+            pastTurns
+                |> List.filter ((==) O)
+                |> List.length
+
+        xsTurn =
+            numberOfXsTurns <= numberOfOsTurns
+    in
+    if xsTurn then
+        X
+
+    else
+        O
+
+
+winner : Board -> BoardSquareValue
+winner board =
+    let
+        listBoard =
+            boardAsLists board
+
+        wonARow boardSquareValue =
+            listBoard
+                |> List.any (List.all ((==) boardSquareValue))
+
+        wonAColumn boardSquareValue =
+            listBoard
+                |> List.Extra.transpose
+                |> List.any (List.all ((==) boardSquareValue))
+
+        wonBackslash boardSquareValue =
+            listBoard
+                |> List.concat
+                |> List.indexedMap Tuple.pair
+                |> List.filter (\listItemWithIndex -> remainderBy (initialBoardSize + 1) (Tuple.first listItemWithIndex) == 0)
+                |> List.map Tuple.second
+                |> List.all ((==) boardSquareValue)
+
+        wonForwardSlash boardSquareValue =
+            listBoard
+                |> List.concat
+                |> List.indexedMap Tuple.pair
+                |> List.filter (\listItemWithIndex -> remainderBy (initialBoardSize - 1) (Tuple.first listItemWithIndex) == 0 && (Tuple.first listItemWithIndex /= 0) && (Tuple.first listItemWithIndex /= initialBoardSize ^ 2 - 1))
+                |> List.map Tuple.second
+                |> List.all ((==) boardSquareValue)
+
+        won boardSquareValue =
+            wonARow boardSquareValue
+                || wonAColumn boardSquareValue
+                || wonBackslash boardSquareValue
+                || wonForwardSlash boardSquareValue
+    in
+    if X |> won then
+        X
+
+    else if O |> won then
+        O
+
+    else
+        Empty
 
 
 
 -- VIEW
 
 
+boardSquareValueToString : BoardSquareValue -> String
+boardSquareValueToString boardSquareValue =
+    case boardSquareValue of
+        X ->
+            "X"
+
+        O ->
+            "O"
+
+        Empty ->
+            ""
+
+
+boardSquareHtml : Int -> Int -> BoardSquareValue -> Html BrowserInteraction
+boardSquareHtml boardRowIndex boardColumnIndex boardSquareValue =
+    Html.td
+        [ style "border" "1px solid #000"
+        , style "border-collapse" "collapse"
+        ]
+        [ Html.button
+            [ style "width" "100px"
+            , style "height" "100px"
+            , style "background" "none"
+            , style "border" "none"
+            , onClick (Clicked ( boardRowIndex, boardColumnIndex ))
+            ]
+            [ Html.span
+                [ style "font-size" "30pt" ]
+                [ boardSquareValue |> boardSquareValueToString |> Html.text ]
+            ]
+        ]
+
+
 boardHtml : Board -> Html BrowserInteraction
 boardHtml board =
     Html.div []
         [ Html.table [ style "border" "1px solid #000", style "border-collapse" "collapse" ]
-            (Array.toList (Array.map (\boardRow -> Html.tr [] (Array.toList (Array.map (\boardCell -> Html.td [] []) boardRow))) board))
+            (Array.toList
+                (Array.indexedMap
+                    (\boardRowIndex boardRow ->
+                        Html.tr []
+                            (Array.toList
+                                (Array.indexedMap
+                                    (\boardColumnIndex boardSquareValue ->
+                                        boardSquareHtml boardRowIndex boardColumnIndex boardSquareValue
+                                    )
+                                    boardRow
+                                )
+                            )
+                    )
+                    board
+                )
+            )
+        , Html.text ("Winner: " ++ Debug.toString (winner board))
+        , Html.button [ onClick ClickedReset ] [ Html.text "Reset" ]
         ]
